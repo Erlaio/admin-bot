1012af2857b223095e272be28ed8a3603ea1e035
kaucap <kaucap@bk.ru>
2022-07-13 14:26:56 +0600

    initial commit

---
+++ utils/set_bot_commands.py
@@ -0,0 +1,8 @@
+from aiogram import types
+
+
+async def set_default_commands(dp):
+    await dp.bot.set_my_commands([
+        types.BotCommand("start", "Запустить бота 💻"),
+        types.BotCommand("help", "Помощь 📣"),
+    ])
