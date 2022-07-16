4b65f2d32c90f97d82b38f7b9397746b4b98b5cd
Andrey3 Hatskevich <a3.hatskevich@a1qa.com>
2022-07-16 16:40:40 +0400

    refactoring

---
+++ keyboard/default/button_value.py
@@ -0,0 +1,12 @@
+class ButtonValue:
+    READ_RULES = 'Ознакомиться с правилами 🤓'
+    DONT_READ_RULES = 'Я не буду читать правила 😐'
+    AGREE_WITH_RULES = 'Я согласен с правилами 😎'
+    DONT_AGREE_WITH_RULES = 'Я не согласен с правилами 🤔'
+    MALE_GENDER = 'Мужской 👨'
+    FEMALE_GENDER = 'Женский 👩‍🦰'
+    WANT_UPLOAD_PHOTO = 'Да! Хочу загрузить свою фоточку 😎'
+    DONT_WANT_UPLOAD_PHOTO = 'Нет, не буду загружать свое фото 🙂'
+    YES = 'Да ✅'
+    NO = 'Нет ❌'
+    CHECK_ACCESS = 'Проверить состояние анкеты ✅'
