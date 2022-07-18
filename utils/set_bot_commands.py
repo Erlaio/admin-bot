44a205d27c89b221036596ae1e1ffcc2be732592
M1nZ <maxykl38@gmail.com>
2022-07-18 13:44:50 +0300

    + command "show_departement_cards"

--- utils/set_bot_commands.py
+++ utils/set_bot_commands.py
@@ -6,4 +6,5 @@ async def set_default_commands(dp):
         types.BotCommand("start", "Запустить бота 💻"),
         types.BotCommand("help", "Помощь 📣"),
         types.BotCommand("show_card", 'Показать карточку пользователя'),
+        types.BotCommand("show_department_cards", 'Показать карточку пользователей отдела')
     ])
