9e5f56f0a97fe32334bb081d5a80b525a1efab24
M1nZ <maxykl38@gmail.com>
2022-07-19 22:11:53 +0300

    test pagely (first stage)

--- handlers/show_user_card_from_bd.py
+++ handlers/show_user_card_from_bd.py
@@ -34,7 +34,7 @@ async def show_user_choice(message: types.Message, state: FSMContext):
                           f'Цели: {i_user.goals}\n' \
                           f'Описание лида: {i_user.lead_description}\n' \
                           f'Время присоединения: {i_user.join_time}\n' \
-                          f'Принят: {i_user.is_approved}\n'                 # Обсудить вывод
+                          f'Принят: {i_user.is_approved}\n'
                 if i_user.is_moderator == 1:
                     caption += 'Является модератором: Модератор\n'
                 try:
@@ -50,6 +50,26 @@ async def show_user_choice(message: types.Message, state: FSMContext):
         await message.answer('Введите id', reply_markup=ReplyKeyboardRemove())
         await UserCardState.user_id.set()
 
+    elif answer == 'Показать всех постранично':
+        user_list = get_all_users()
+        if user_list:
+            index = 0
+            for _ in range(len(user_list) // 2):
+                result = ''
+                for i_user in user_list[index:index + 2]:
+                    caption = f'ФИО: {i_user.surname} {i_user.name} {i_user.patronymic}\n' \
+                              f'Пол: {i_user.gender}\n' \
+                              f'Логин в Telegram: {i_user.tg_login}\n' \
+                              f'Желаемый отдел: {i_user.desired_department}\n' \
+                              f'Скилы: {i_user.skills}\n' \
+                              f'Цели: {i_user.goals}\n' \
+                              f'Описание лида: {i_user.lead_description}\n' \
+                              f'Время присоединения: {i_user.join_time}\n' \
+                              f'Принят: {i_user.is_approved}\n\n'
+                    result += caption
+                index += 2
+                await bot.send_message(message.chat.id, result, reply_markup=ReplyKeyboardRemove())
+
 
 @dp.message_handler(state=UserCardState.user_id)
 async def show_user_by_id(message: types.Message, state: FSMContext):
@@ -64,7 +84,7 @@ async def show_user_by_id(message: types.Message, state: FSMContext):
                   f'Цели: {user.goals}\n' \
                   f'Описание лида: {user.lead_description}\n' \
                   f'Время присоединения: {user.join_time}\n' \
-                  f'Принят: {user.is_approved}\n'                       # Обсудить вывод
+                  f'Принят: {user.is_approved}\n'
         if user.is_moderator == 1:
             caption += 'Является модератором: Модератор\n'
         try:
