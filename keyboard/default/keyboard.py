6cae19ba03ba16d5639357897b6e0d0f51cfa1af
M1nZ <maxykl38@gmail.com>
2022-07-20 12:46:28 +0300

    +inline objects

--- keyboard/default/keyboard.py
+++ keyboard/default/keyboard.py
@@ -1,4 +1,4 @@
-from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardMarkup, InlineKeyboardButton
 
 from keyboard.default.button_value import ButtonValue as button
 
