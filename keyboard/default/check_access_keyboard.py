534e02566174d000abf66c77e68ba52c6f0aab6a
M1nZ <maxykl38@gmail.com>
2022-07-27 20:44:58 +0300

    [+] Клавиатура проверки состояния анкеты перенесена в отдельный файл

--- keyboard/default/check_access_keyboard.py
+++ keyboard/default/check_access_keyboard.py
@@ -3,10 +3,12 @@ from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 
 
 class CheckAccessKeyboard(ButtonFactory):
+    CHECK_ACCESS = 'Проверить состояние анкеты ✅'
+
     KEYBOARD = ReplyKeyboardMarkup(
         keyboard=[
             [
-                KeyboardButton(text='Проверить состояние анкеты ✅'),
+                KeyboardButton(text=CHECK_ACCESS),
             ]
         ],
         resize_keyboard=True
