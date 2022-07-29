105123efdde95741125e6eae6804fe10dddd6d75
Marina Chernysheva <marich.99@mail.ru>
2022-07-29 23:40:52 +0300

    [+] Добавлена команда /help

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,4 +1,5 @@
 from .start import dp
+from .help import dp
 from .show_card import dp
 from .show_department_cards import dp
 from .department_handler import dp
---
+++ handlers/help.py
@@ -0,0 +1,13 @@
+from loader import dp
+from aiogram.dispatcher.filters.builtin import CommandHelp
+from aiogram import types
+
+
+@dp.message_handler(CommandHelp(), state="*")
+async def start_handler(message: types.Message):
+    command_bot = '/start - начать работу с ботом.\n' \
+                  '/department - добавить новый отдел или изменить данные о существующем.\n' \
+                  '/project -  добавить новый проект или изменить данные о существующем.\n' \
+                  '/show_card - посмотреть личные карточки студентов Школы IT.\n' \
+                  '/show_department_cards - вывести список всех участников отдела.\n'
+    await message.answer(command_bot)
