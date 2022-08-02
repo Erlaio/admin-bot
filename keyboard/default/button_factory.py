740a5f003c41504f49d64bc8c130cfab1bc68ed0
Khalil Batkaev <xermed@yandex.ru>
2022-08-02 23:21:52 +0300

    [~] Изменил class StopBotKeyboard

--- keyboard/default/button_factory.py
+++ keyboard/default/button_factory.py
@@ -11,8 +11,6 @@ class ButtonFactory(ABC):
         key_list = [KeyboardButton(getattr(cls, i_const)) for i_const in dir(cls)[::-1]
                     if any([i_const.isupper(), i_const.isdigit()])]
 
-        key_list.append(KeyboardButton('Вернуться в начало'))       # TODO убрать
-
         row = 3 if len(key_list) < 5 else math.ceil(len(key_list) / 2)
 
         keyboard = ReplyKeyboardMarkup(resize_keyboard=True, row_width=row).add(*key_list)
--- keyboard/default/keyboards.py
+++ keyboard/default/keyboards.py
@@ -4,7 +4,11 @@ from pkg.db.department_func import get_all_departments
 from .button_factory import ButtonFactory
 
 
-class DepartmentsKeyboard(ButtonFactory):
+class StopBotKeyboard(ButtonFactory):
+    STOP_BOT = 'Вернуться в начало'
+
+
+class DepartmentsKeyboard(StopBotKeyboard):
 
     @classmethod
     async def __get_department(cls) -> None:
@@ -18,7 +22,7 @@ class DepartmentsKeyboard(ButtonFactory):
         return super().get_reply_keyboard()
 
 
-class AgreementKeyboard(ButtonFactory):
+class AgreementKeyboard(StopBotKeyboard):
     AGREE_WITH_RULES = 'Я согласен с правилами 😎'
     DONT_AGREE_WITH_RULES = 'Я не согласен с правилами 🤔'
 
@@ -27,45 +31,41 @@ class CheckAccessKeyboard(ButtonFactory):           # TODO без stop
     CHECK_ACCESS = 'Проверить состояние анкеты ✅'
 
 
-class ChoiceKeyboard(ButtonFactory):
+class ChoiceKeyboard(StopBotKeyboard):
     READ_RULES = 'Ознакомиться с правилами 🤓'
     DONT_READ_RULES = 'Я не буду читать правила 😐'
 
 
-class DepartmentCommandsKeyboard(ButtonFactory):
+class DepartmentCommandsKeyboard(StopBotKeyboard):
     CREATE_DEPARTMENT = 'Создать новый отдел'
     DELETE_DEPARTMENT = 'Удалить отдел'
     CHANGE_DEPARTMENT_NAME = 'Сменить имя отдела'
     CHANGE_DEPARTMENT_LEAD = 'Сменить/добавить тим лида отдела'
 
 
-class GenderKeyboard(ButtonFactory):
+class GenderKeyboard(StopBotKeyboard):
     MALE_GENDER = 'Мужской 👨'
     FEMALE_GENDER = 'Женский 👩‍🦰'
 
 
-class PhotoKeyboard(ButtonFactory):
+class PhotoKeyboard(StopBotKeyboard):
     WANT_UPLOAD_PHOTO = 'Да! Хочу загрузить свою фоточку 😎'
     DONT_WANT_UPLOAD_PHOTO = 'Нет, не буду загружать свое фото 🙂'
 
 
-class ProjectCommandsKeyboard(ButtonFactory):
+class ProjectCommandsKeyboard(StopBotKeyboard):
     CREATE_PROJECT = 'Создать новый проект'
     DELETE_PROJECT = 'Удалить проект'
     CHANGE_PROJECT_NAME = 'Сменить имя проекта'
     CHANGE_PROJECT_LEAD = 'Сменить/добавить тим лида проекта'
 
 
-class ShowUserKeyboard(ButtonFactory):
+class ShowUserKeyboard(StopBotKeyboard):
     VIEW_ALL = 'Посмотреть всех'
     VIEW_ID = 'Посмотреть по ID'
     VIEW_TG_LOGIN = 'Посмотреть по Логину в Telegram'
 
 
-class YesNoKeyboard(ButtonFactory):
+class YesNoKeyboard(StopBotKeyboard):
     YES = 'Да ✅'
     NO = 'Нет ❌'
-
-
-class StopBotKeyboard(ButtonFactory):
-    pass
