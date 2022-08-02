148660de0f2c0699359941bdcc99f609f87f390b
Erlaio <uransusuu@yandex.ru>
2022-08-02 19:11:28 +0300

    [+] исправлена ошибка при получении id модератора

--- utils/get_moder_chat_id.py
+++ utils/get_moder_chat_id.py
@@ -10,7 +10,6 @@ class ModeratorUtils:
         for moderator_id in moderator_id_list:
             if moderator_id not in cls.BLACK_LIST:
                 cls.BLACK_LIST.append(moderator_id)
+                if len(moderator_id_list) == len(cls.BLACK_LIST):
+                    cls.BLACK_LIST = []
                 return moderator_id
-
-        if len(moderator_id_list) == len(cls.BLACK_LIST):
-            cls.BLACK_LIST = []
