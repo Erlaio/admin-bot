4d608e0792f4e78114a6a5970861cd027a87d370
Khalil Batkaev <xermed@yandex.ru>
2022-08-03 00:09:36 +0300

    [~] Отображение кнопки stop в отдельной строке

--- keyboard/default/button_factory.py
+++ keyboard/default/button_factory.py
@@ -9,10 +9,12 @@ class ButtonFactory(ABC):
     @classmethod
     def get_reply_keyboard(cls) -> ReplyKeyboardMarkup:
         key_list = [KeyboardButton(getattr(cls, i_const)) for i_const in dir(cls)[::-1]
-                    if any([i_const.isupper(), i_const.isdigit()])]
+                    if i_const != 'STOP_BOT' and any([i_const.isupper(), i_const.isdigit()])]
+        stop_button = [KeyboardButton(getattr(cls, i_const)) for i_const in dir(cls) if i_const == 'STOP_BOT']
 
         row = 3 if len(key_list) < 5 else math.ceil(len(key_list) / 2)
 
         keyboard = ReplyKeyboardMarkup(resize_keyboard=True, row_width=row).add(*key_list)
+        keyboard.add(*stop_button)
 
         return keyboard
