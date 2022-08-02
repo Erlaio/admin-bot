bd40054258f490e8331e788eb751323fe7408b0e
M1nZ <maxykl38@gmail.com>
2022-08-02 19:41:34 +0300

    [~] Расширенный вывод в /help (больше команд + форматрование)

--- handlers/help.py
+++ handlers/help.py
@@ -7,10 +7,15 @@ from aiogram import types
 
 @dp.message_handler(CommandHelp())
 async def start_handler(message: types.Message, state: FSMContext):
-    command_bot = '/start - начать работу с ботом.\n' \
-                  '/department - добавить новый отдел или изменить данные о существующем.\n' \
-                  '/project -  добавить новый проект или изменить данные о существующем.\n' \
-                  '/show_card - посмотреть личные карточки студентов Школы IT.\n' \
-                  '/show_department_cards - вывести список всех участников отдела.\n'
+    command_bot = '<b>Общие команды:</b>\n\n' \
+                  '/start - начать работу с ботом.\n\n' \
+                  '/help - Помощь по командам взаимодействия с ботом.\n\n' \
+                  '/rules - Правила Школы IT.\n\n\n' \
+                  '<b>Команды модераторов:</b>\n\n' \
+                  '/department - добавить новый отдел или изменить данные о существующем.\n\n' \
+                  '/project -  добавить новый проект или изменить данные о существующем.\n\n' \
+                  '/show_card - посмотреть личные карточки студентов Школы IT.\n\n' \
+                  '/show_department_cards - вывести список всех участников отдела.\n\n' \
+                  '/review_cards - работа со всеми неапрувнутыми учениками'
     await message.answer(command_bot)
     await state.finish()
