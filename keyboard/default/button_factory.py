59abad0b9fd2f7e2383271c0f534d957913b3704
M1nZ <maxykl38@gmail.com>
2022-08-03 18:27:32 +0300

    [+] Добавлена кнопка "Вернуться на главную" (по умолчанию появляется на новой строке)

--- keyboard/default/button_factory.py
+++ keyboard/default/button_factory.py
@@ -7,14 +7,15 @@ from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 class ButtonFactory(ABC):
 
     @classmethod
-    def get_reply_keyboard(cls) -> ReplyKeyboardMarkup:
+    def get_reply_keyboard(cls, add_stop=True) -> ReplyKeyboardMarkup:
         key_list = [KeyboardButton(getattr(cls, i_const)) for i_const in dir(cls)[::-1]
-                    if i_const != 'STOP_BOT' and any([i_const.isupper(), i_const.isdigit()])]
-        stop_button = [KeyboardButton(getattr(cls, i_const)) for i_const in dir(cls) if i_const == 'STOP_BOT']
+                    if any([i_const.isupper(), i_const.isdigit()])]
 
         row = 3 if len(key_list) < 5 else math.ceil(len(key_list) / 2)
 
         keyboard = ReplyKeyboardMarkup(resize_keyboard=True, row_width=row).add(*key_list)
-        keyboard.add(*stop_button)
+
+        if add_stop:
+            keyboard.row(KeyboardButton('Вернуться на главную'))
 
         return keyboard
