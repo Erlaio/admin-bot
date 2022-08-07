0cb9fa46c9e90ded48f3c5608c27dab67e2721b7
Khalil Batkaev <xermed@yandex.ru>
2022-08-07 21:10:40 +0300

    [~] Обернул удаление участников в try/except

--- utils/delete_user.py
+++ utils/delete_user.py
@@ -1,8 +1,12 @@
+from aiogram.utils.exceptions import NotEnoughRightsToRestrict
 from pkg.db.user_func import delete_user_by_id
 from loader import bot
 
 
 async def delete_user(user_id, channels):
     for channel in channels:
-        await bot.kick_chat_member(chat_id=channel, user_id=user_id)
+        try:
+            await bot.kick_chat_member(chat_id=channel, user_id=user_id)
+        except NotEnoughRightsToRestrict as e:
+            print(e)
     await delete_user_by_id(user_id)
