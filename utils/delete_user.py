55a0b0259f6306d9da6c3b69ec5382978b10ae75
M1nZ <maxykl38@gmail.com>
2022-08-08 22:34:35 +0300

    [+] Обработка BadRequest (в основном вызывалась, если нет юзера в группе)

--- utils/delete_user.py
+++ utils/delete_user.py
@@ -1,5 +1,5 @@
-from aiogram.utils.exceptions import NotEnoughRightsToRestrict
-from pkg.db.user_func import delete_user_by_id, delete_user_by_tg_id
+from aiogram.utils.exceptions import NotEnoughRightsToRestrict, BadRequest
+from pkg.db.user_func import delete_user_by_tg_id
 from loader import bot
 
 
@@ -8,5 +8,7 @@ async def delete_user(user_id, channels):
         try:
             await bot.kick_chat_member(chat_id=channel, user_id=user_id)
         except NotEnoughRightsToRestrict as e:
+            print(e)
+        except BadRequest as e:
             print(e)
     await delete_user_by_tg_id(telegram_id=user_id)
