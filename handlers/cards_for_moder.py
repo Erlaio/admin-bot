8a8bde46be13b3480b5b0e67acce3ca2acf7331f
M1nZ <maxykl38@gmail.com>
2022-09-20 15:29:41 +0300

    [+] Обработка изменений полей в бд (раньше была заглушка, теперь функционал без учета переключений страниц)

--- handlers/cards_for_moder.py
+++ handlers/cards_for_moder.py
@@ -6,24 +6,27 @@ from keyboard.default.inline_keyboards import ModeratorInlineKeyboard, BackInlin
     ModeratorChangeCardInlineKeyboard
 from keyboard.default.pagination import Pagination, InlineKeyboardButton
 from loader import dp, bot
-from pkg.db.user_func import get_user_by_tg_id, get_all_users
+from pkg.db.user_func import get_user_by_tg_id, get_all_users, update_field_value
+from utils.context_helper import ContextHelper
 from utils.send_card import send_card
 
 
 @dp.message_handler(commands='change_card_by_moder')
 async def change_card_by_moder(message: types.Message, state: FSMContext):
     # try:
-        user = await get_user_by_tg_id(message.from_user.id)
-        if user.is_moderator:
-            await send_character_page_for_edit(message)
-        else:
-            await message.answer('Вы не модератор',
-                                 reply_markup=ReplyKeyboardRemove())
-            await state.finish()
-    # except (TypeError, AttributeError):
-    #     await message.answer('Вас нет в базе, пожалуйста пройдите регистрацию',
-    #                          reply_markup=ReplyKeyboardRemove())
-    #     await state.finish()
+    user = await get_user_by_tg_id(message.from_user.id)
+    if user.is_moderator:
+        await send_character_page_for_edit(message)
+    else:
+        await message.answer('Вы не модератор',
+                             reply_markup=ReplyKeyboardRemove())
+        await state.finish()
+
+
+# except (TypeError, AttributeError):
+#     await message.answer('Вас нет в базе, пожалуйста пройдите регистрацию',
+#                          reply_markup=ReplyKeyboardRemove())
+#     await state.finish()
 
 
 async def send_character_page_for_edit(message: types.Message, page=1):
@@ -32,13 +35,15 @@ async def send_character_page_for_edit(message: types.Message, page=1):
         paginator = Pagination(
             len(user_list),
             current_page=page,
-            data_pattern='character_for_edit#{page}'
+            data_pattern='change#{page}'
         )
         user = user_list[page - 1]
-        paginator.add_before(
-            *ModeratorChangeCardInlineKeyboard(page, user).get_inline_keyboard(is_key=True))
+        list_of_buttons = ModeratorChangeCardInlineKeyboard(page, user).get_inline_keyboard(is_key=True)
+        for buttons in list_of_buttons:
+            paginator.add_before(
+                *buttons)
         paginator.add_after(
-            *BackInlineKeyboard().get_inline_keyboard(is_key=True)
+            *BackInlineKeyboard().get_inline_keyboard(is_key=True)[0]
         )
         await send_card(
             message.chat.id,
@@ -46,14 +51,28 @@ async def send_character_page_for_edit(message: types.Message, page=1):
             reply_markup=paginator.markup,
         )
     else:
-        await message.answer('Некого апрувить', reply_markup=ReplyKeyboardRemove())
+        await message.answer('Пользователей нет в базе данных', reply_markup=ReplyKeyboardRemove())
 
 
-@dp.callback_query_handler(lambda call: call.data.split('#')[0] == 'character_for_edit')
-async def characters_for_edit_page_callback(call):
-    page = int(call.data.split('#')[1])
+@dp.callback_query_handler(lambda call: call.data.split('#')[0] == 'change')
+async def characters_for_edit_page_callback(call: types.CallbackQuery, state: FSMContext):
+    _, page, field_name, field_value, telegram_id = call.data.split('#')
     await bot.delete_message(
         call.message.chat.id,
         call.message.message_id
     )
-    await send_character_page_for_edit(call.message, page)
+    await ContextHelper.add_tg_id(telegram_id=telegram_id, context=state)
+    await ContextHelper.add_some_data(data=field_name, context=state)
+    await bot.send_message(call.message.chat.id, f'Выбрано поле {field_name} со значением {field_value}')
+    await state.set_state('change')
+
+
+@dp.message_handler(state='change')
+async def change_data_of_user(message: types.Message, state: FSMContext):
+    telegram_id = await ContextHelper.get_tg_id(state)
+    field_name = await ContextHelper.get_some_data(state)
+    answer = message.text
+    await update_field_value(telegram_id=telegram_id, field=field_name, value=answer)
+    await bot.send_message(chat_id=message.chat.id,
+                           text=f'Описание было изменено на {answer}')
+    await state.finish()
