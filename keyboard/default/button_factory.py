350e7446c060febc419e626573a08fc7a8d30418
M1nZ <maxykl38@gmail.com>
2022-09-20 15:27:58 +0300

    [+] Ограничение вывода InlineButtons в 3 кнопки в ряду

--- keyboard/default/button_factory.py
+++ keyboard/default/button_factory.py
@@ -43,7 +43,12 @@ class ButtonFactory(ABC):
                         InlineKeyboardButton(
                             text=j_key, callback_data=j_value))
 
-        return inline_keyboard
+        upd_inline_keyboard = []
+        index = 0
+        for _ in range(math.ceil(len(inline_keyboard) / 3)):
+            upd_inline_keyboard.append(inline_keyboard[index:index + 3])
+            index += 3
+        return upd_inline_keyboard
 
     @classmethod
     def get_stop_message(cls):
