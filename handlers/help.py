d3231b263b086899b893e9d81bd5cbb6b72885d6
M1nZ <maxykl38@gmail.com>
2022-09-25 23:26:06 +0300

    [+] Добавлена команда /moder

--- handlers/help.py
+++ handlers/help.py
@@ -9,13 +9,10 @@ from aiogram import types
 async def start_handler(message: types.Message, state: FSMContext):
     command_bot = '<b>Общие команды:</b>\n\n' \
                   '/start - начать работу с ботом.\n\n' \
-                  '/help - Помощь по командам взаимодействия с ботом.\n\n' \
-                  '/rules - Правила Школы IT.\n\n\n' \
-                  '<b>Команды модераторов:</b>\n\n' \
-                  '/department - добавить новый отдел или изменить данные о существующем.\n\n' \
-                  '/project -  добавить новый проект или изменить данные о существующем.\n\n' \
+                  '/help - помощь по командам взаимодействия с ботом.\n\n' \
                   '/show_card - посмотреть личные карточки студентов Школы IT.\n\n' \
-                  '/show_department_cards - вывести список всех участников отдела.\n\n' \
-                  '/review_cards - работа со всеми неапрувнутыми учениками'
+                  '/show_department_cards - показать карточки пользователей отдела.\n\n' \
+                  '/moder - меню модератора.\n\n' \
+                  '/rules - правила Школы IT.\n\n\n'
     await message.answer(command_bot)
     await state.finish()
