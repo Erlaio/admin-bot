792432bc84814e9491639308303468a3f3b30f54
aanatyrnal <cacina_ang1891@mail.ru>
2022-10-11 16:30:01 +0300

    [~] возврат версии с данными в cfg и settings

--- pkg/settings/settings.py
+++ pkg/settings/settings.py
@@ -1,5 +1,5 @@
 import pydantic
-from dotenv import find_dotenv
+from cfg.cfg import config as cfg
 
 
 class _Settings(pydantic.BaseSettings):
@@ -7,22 +7,21 @@ class _Settings(pydantic.BaseSettings):
         env_file_encoding = "utf-8"
 
 
-class Settings(_Settings):
+class Settings(Settings):
     # PostgresQL
-    #BOT_KEY: pydantic.SecretStr
-    POSTGRES_HOSTNAME: str
-    POSTGRES_DATABASE: str
-    POSTGRES_USER: str
-    POSTGRES_PASSWORD: str
-    POSTGRES_PORT: int
+    POSTGRES_HOSTNAME: str = cfg.POSTGRES_HOSTNAME
+    POSTGRES_DATABASE: str = cfg.POSTGRES_DATABASE
+    POSTGRES_USER: str = cfg.POSTGRES_USER
+    POSTGRES_PASSWORD: str = cfg.POSTGRES_PASSWORD
+    POSTGRES_PORT: int = cfg.POSTGRES_PORT
 
     # BaseSettings
     # SQLITE_FILENAME: str = cfg.SQLITE_FILENAME
-    SECRET_KEY: str
-    TELEGRAM_MODERS_CHAT_ID: int
-    TELEGRAM_SCHOOL_CHATS: list
+    SECRET_KEY: str = cfg.SECRET_KEY
+    TELEGRAM_MODERS_CHAT_ID: int = cfg.TELEGRAM_MODERS_CHAT_ID
+    TELEGRAM_SCHOOL_CHATS: list = cfg.TELEGRAM_SCHOOL_CHATS
 
 
 def _get_settings() -> Settings:
-    settings = Settings(_env_file=find_dotenv('.env'))
-    return settings
+    settings = Settings()
+
