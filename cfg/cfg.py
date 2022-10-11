8f8f22d8ac108475fb447d278302cfdd9dae30d1
aanatyrnal <cacina_ang1891@mail.ru>
2022-10-11 15:11:32 +0300

    [+] версия вынесения данных в .env

--- cfg/cfg.py
+++ cfg/cfg.py
@@ -8,15 +8,15 @@ from envreader import Field
 
 class Config(EnvReader):
     # SQLITE_FILENAME: str = Field('blah.db', description="sqlite3 database file path")
-    SECRET_KEY: str = Field('asdf', description="Admin Bot Secret Key")
-    TELEGRAM_MODERS_CHAT_ID: int = Field(-1001658648627, description="Moders_IDs")
-    TELEGRAM_SCHOOL_CHATS: list = Field([-1001658648627, -1001770112839], description="School_Chat_ID")
+    SECRET_KEY: str = Field(..., description="Admin Bot Secret Key")
+    TELEGRAM_MODERS_CHAT_ID: int = Field(..., description="Moders_IDs")
+    TELEGRAM_SCHOOL_CHATS: list = Field(..., description="School_Chat_ID")
 
-    POSTGRES_HOSTNAME: str = Field('localhost', description='hostname')
-    POSTGRES_DATABASE: str = Field('testdb', description='db_name')
-    POSTGRES_USER: str = Field('postgres', description='user_name')
-    POSTGRES_PASSWORD: str = Field('4er4er4er345', description='password')
-    POSTGRES_PORT: int = Field('5432', description='port_number')
+    POSTGRES_HOSTNAME: str = Field(..., description='hostname')
+    POSTGRES_DATABASE: str = Field(..., description='db_name')
+    POSTGRES_USER: str = Field(..., description='user_name')
+    POSTGRES_PASSWORD: str = Field(..., description='password')
+    POSTGRES_PORT: int = Field(..., description='port_number')
 
 
 try:
--- pkg/settings/settings.py
+++ pkg/settings/settings.py
@@ -1,23 +1,28 @@
 import pydantic
+from dotenv import find_dotenv
 
-from cfg.cfg import config as cfg
+
+class _Settings(pydantic.BaseSettings):
+    class Config:
+        env_file_encoding = "utf-8"
 
 
-class Settings:
+class Settings(_Settings):
     # PostgresQL
-    POSTGRES_HOSTNAME: str = cfg.POSTGRES_HOSTNAME
-    POSTGRES_DATABASE: str = cfg.POSTGRES_DATABASE
-    POSTGRES_USER: str = cfg.POSTGRES_USER
-    POSTGRES_PASSWORD: str = cfg.POSTGRES_PASSWORD
-    POSTGRES_PORT: int = cfg.POSTGRES_PORT
+    #BOT_KEY: pydantic.SecretStr
+    POSTGRES_HOSTNAME: str
+    POSTGRES_DATABASE: str
+    POSTGRES_USER: str
+    POSTGRES_PASSWORD: str
+    POSTGRES_PORT: int
 
     # BaseSettings
     # SQLITE_FILENAME: str = cfg.SQLITE_FILENAME
-    SECRET_KEY: str = cfg.SECRET_KEY
-    TELEGRAM_MODERS_CHAT_ID: int = cfg.TELEGRAM_MODERS_CHAT_ID
-    TELEGRAM_SCHOOL_CHATS: list = cfg.TELEGRAM_SCHOOL_CHATS
+    SECRET_KEY: str
+    TELEGRAM_MODERS_CHAT_ID: int
+    TELEGRAM_SCHOOL_CHATS: list
 
 
 def _get_settings() -> Settings:
-    settings = Settings()
+    settings = Settings(_env_file=find_dotenv('.env'))
     return settings
